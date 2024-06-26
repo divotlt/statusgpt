9efb1b984712de4823b2aa8214a232f05235436a
divotlt <150188672+divotlt@users.noreply.github.com>
2024-06-26 02:45:59 +0000

    import

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Chatting","Moderating","Helping","Created by the Scarlet Team"];
 
 
 let currentIndex = 0;
